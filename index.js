934f95782e3e03550804cdee2cff0705b82fd2a2
mdshabbualam <mdshabbu6776@gmail.com>
2026-06-25 18:01:23 +0530

    i add few line

---
+++ index.js
@@ -0,0 +1,23 @@
+// how to create Object;
+let car= {
+
+    model: 'thar',
+    year: '2000',
+    color: 'dark black'
+}
+console.log(car)
+console.log(car.model)
+console.log(car.color)
+
+let a=6;
+    b=6;
+    c=a+b;
+    console.log(c);
+//  how to creat Array
+    let names=[ 'apple','watermelone','papaya','banana']
+    console.log(names);
+    console.log(names[0,1,2,3])
+    console.log(names[2]);
+    console.log(names[1]);
+    names[2]='guava';
+    console.log(names);

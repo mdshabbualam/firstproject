d52ab4ee187c2b053812dc88839b7523954ac2e7
mdshabbualam <mdshabbu6776@gmail.com>
2026-06-27 17:52:47 +0530

    make mini calculator

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-// how to create Object;
-let car= {
+// // how to create Object;
+// let car= {
 
-    model: 'thar',
-    year: '2000',
-    color: 'dark black'
+//     model: 'thar',
+//     year: '2000',
+//     color: 'dark black'
+// }
+// console.log(car)
+// console.log(car.model)
+// console.log(car.color)
+
+// let a=6;
+//     b=6;
+//     c=a+b;
+//     console.log(c);
+// //  how to creat Array
+//     let names=[ 'apple','watermelone','papaya','banana']
+//     console.log(names);
+//     console.log(names[0,1,2,3])
+//     console.log(names[2]);
+//     console.log(names[1]);
+//     names[2]='guava';
+//     console.log(names);
+//     names[2]='grapes';
+//     console.log(names);
+//     console.log(names[2]);
+//     names[4]='pinapple';
+//     console.log(names);
+
+function add(){
+    let a= parseInt(document.getElementById("num1").value);
+     let b= parseInt(document.getElementById("num2").value);
+     let sum= a+b;
+     document.getElementById("data").innerHTML=sum;
 }
-console.log(car)
-console.log(car.model)
-console.log(car.color)
-
-let a=6;
-    b=6;
-    c=a+b;
-    console.log(c);
-//  how to creat Array
-    let names=[ 'apple','watermelone','papaya','banana']
-    console.log(names);
-    console.log(names[0,1,2,3])
-    console.log(names[2]);
-    console.log(names[1]);
-    names[2]='guava';
-    console.log(names);
-    names[2]='grapes';
-    console.log(names);
-    console.log(names[2]);
-    names[4]='pinapple';
-    console.log(names);
+function sub(){
+    let a= parseInt(document.getElementById("num1").value);
+     let b= parseInt(document.getElementById("num2").value);
+     let sum= a-b;
+     document.getElementById("data").innerHTML=sum;
+}
